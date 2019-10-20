e6981bd784f69011735db75d301f51a8dfd0c36f
Stefan Wilkes <stefan.wilkes@gmail.com>
2019-10-20 18:16:21 +0200

    WIP ParameterDialog

--- HomematicCore.Homematic.Daemon/Domain/ParameterSet.cs
+++ HomematicCore.Homematic.Daemon/Domain/ParameterSet.cs
@@ -5,5 +5,12 @@ namespace HomematicCore.Homematic.Daemon.Domain
 {
     public class ParameterSet : Dictionary<string, object>
     {
+        public object GetValue(string key) 
+        {
+            object value = null;
+            TryGetValue(key, out value);
+
+            return value;
+        }
     }
 }
